b074575750860eec806c50bd35f74984d5daa0be
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-19 13:21:35 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,76 +32,6 @@ async def on_ready():
     print('Bot is working')
     return await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Flight take another L'))
 
-
-
-@client.event
-async def on_member_join(member):
-    try:
-        embed = discord.Embed(colour=discord.Colour.dark_green(), description=f"What's up {member}, and welcome to the server! You are now member {len(list(member.guild.members))} !")
-        embed.set_thumbnail(url=f"{member.avatar_url}")
-        embed.set_author(name=f"Welcome {member.name}", icon_url=f"{member.avatar_url}")
-        embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
-        embed.timestamp = datetime.datetime.utcnow()
-
-        channel = client.get_channel(id=721630160448782367)
-
-        message = await channel.send(embed=embed)
-        await message.add_reaction("<:callme:729502466592210966>")
-        
-        await asyncio.sleep(1)
-        
-        embed = discord.Embed(title="Welcome to the Server!", description="My prefix is `!` Make sure to **VERIFY** yourself in the (rules-verify) channel to access the rest of the Discord and make sure to read the rules.\nEnjoy your stay 😉", color=discord.Color.dark_blue())
-        await member.send(embed=embed)
-    except Exception as error:
-        raise (error)
-
-@client.event
-async def on_member_remove(member):
-    embed = discord.Embed(colour=discord.Colour.dark_red(), description=f"{member} just left the server. Thanks for visiting! Member Count: {len(list(member.guild.members))}")
-    embed.set_thumbnail(url=f"{member.avatar_url}")
-    embed.set_author(name=f"Goodbye {member.name}", icon_url=f"{member.avatar_url}")
-    embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
-    embed.timestamp = datetime.datetime.utcnow()
-
-    channel = client.get_channel(id=721630160448782367)
-
-    message = await channel.send(embed=embed)
-    await message.add_reaction("<:waving:729500876288557109>")
-
-@client.event
-async def on_raw_reaction_add(payload):
-    message_id = payload.message_id
-    if message_id == 733354307298132078:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        if payload.emoji.name == "🎉":
-            role = discord.utils.get(guild.roles, name="Giveaway")
-        elif payload.emoji.name == "🔔":
-            role = discord.utils.get(guild.roles, name="Announcements")
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(role)
-                print("done")
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    message_id = payload.message_id
-    if message_id == 733354307298132078:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        if payload.emoji.name == "🎉":
-            role = discord.utils.get(guild.roles, name="Giveaway")
-        elif payload.emoji.name == "🔔":
-            role = discord.utils.get(guild.roles, name="Announcements")
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(role)
-                print("removed")
-
 @client.command(pass_context=True, brief="Shows users balance", aliases=["bal"])
 async def balance(ctx, member: discord.Member = None):
     USER_ID = ctx.message.author.id
@@ -115,7 +45,7 @@ async def balance(ctx, member: discord.Member = None):
         db.commit()
 
     SQL.execute(f'select balance from Accounts where user_id="{USER_ID}"')
-    result_userbal = QLS.fetchone()
+    result_userbal = SQL.fetchone()
     await ctx.send(f"{ctx.message.author.mention} has a balance of {result_userbal[0]} {C_NAME}")
 
 @client.command(pass_context=True, aliases=["store"])
@@ -206,12 +136,20 @@ async def top10(ctx):
     embed.set_author(name="Top 10 bank accounts")
     embed.add_field(name="#1", value=f"User: {result_top10[0][0]} Bal: {result_top10[0][1]}", inline=False)
     embed.add_field(name="#2", value=f"User: {result_top10[1][0]} Bal: {result_top10[1][1]}", inline=False)
+    embed.add_field(name="#3", value=f"User: {result_top10[2][0]} Bal: {result_top10[2][2]}", inline=False)
+    embed.add_field(name="#4", value=f"User: {result_top10[3][0]} Bal: {result_top10[3][3]}", inline=False)
+    embed.add_field(name="#5", value=f"User: {result_top10[4][0]} Bal: {result_top10[4][4]}", inline=False)
+    embed.add_field(name="#6", value=f"User: {result_top10[5][0]} Bal: {result_top10[5][5]}", inline=False)
+    embed.add_field(name="#7", value=f"User: {result_top10[6][0]} Bal: {result_top10[6][6]}", inline=False)
+    embed.add_field(name="#8", value=f"User: {result_top10[7][0]} Bal: {result_top10[7][7]}", inline=False)
+    embed.add_field(name="#9", value=f"User: {result_top10[8][0]} Bal: {result_top10[8][8]}", inline=False)
+    embed.add_field(name="#10", value=f"User: {result_top10[9][0]} Bal: {result_top10[9][9]}", inline=False)
 
     await ctx.send(embed=embed)
 
 
 
-extension = ['cogs.help_command', 'cogs.General Commands', 'cogs.Fun Commands', 'cogs.mc', 'cogs.Suggestions', 'cogs.Application', 'cogs.Moderation', 'cogs.Administrator Commands', 'cogs.global']
+extension = ['cogs.help_command', 'cogs.General Commands', 'cogs.Fun Commands', 'cogs.mc', 'cogs.Suggestions', 'cogs.Application', 'cogs.Moderation', 'cogs.Administrator Commands', 'cogs.other.global', 'cogs.events.reactions', 'cogs.events.welcome']
 
 if __name__ == '__main__':
     for ext in extension:
