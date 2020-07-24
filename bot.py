24a099802a04e3c0864f480308a49fd16b814800
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-24 11:39:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,17 @@
 import discord
 from discord.ext import commands
+import random
+from random import randint
+import platform
+import logging
 import asyncio
+import datetime
+import typing
+import sqlite3
+import aiosqlite
+import os
+from discord.ext import commands
+from discord.utils import get
 
 client = commands.Bot(command_prefix = '!', case_insensitive=True)
 client.remove_command('help')
@@ -10,8 +21,7 @@ async def on_ready():
     print('Bot is working')
     return await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Flight take another L'))
 
-extension = ['cogs.help_command', 'cogs.General Commands', 'cogs.Fun Commands', 'cogs.mc', 'cogs.Suggestions', 'cogs.Application', 'cogs.Moderation', 'cogs.Administrator Commands',
- 'cogs.giveaway', 'cogs.other.global', 'cogs.events.reactions', 'cogs.events.welcome', 'cogs.events.errors']
+extension = ['cogs.help_command', 'cogs.General Commands', 'cogs.Fun Commands', 'cogs.mc', 'cogs.Suggestions', 'cogs.Application', 'cogs.Moderation', 'cogs.Administrator Commands', 'cogs.giveaway', 'cogs.other.global', 'cogs.events.reactions', 'cogs.events.welcome', 'cogs.events.errors']
 
 if __name__ == '__main__':
     for ext in extension:
