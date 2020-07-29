dc562475282973b58c95c8508a91ce55805ef93d
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-29 05:55:35 -0400

    Update welcome.py

--- cogs/events/welcome.py
+++ cogs/events/welcome.py
@@ -6,7 +6,21 @@ import asyncio
 class Welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
+    #Sends a fancy embed to show the prefixes    
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+      names = ['gener', 'chat', 'memb', 'welc']
+      channel = discord.utils.find(
+        lambda channel:any(
+          map(lambda w: w in channel.name, names)),
+        guild.text_channels) #When the bot joins a server, this will check for a general or welcome channel, to send an embed
+      #channel = discord.utils.find(lambda **kwargs: kwargs.value())(one='gener', two='chat', three='welc', four='memb', fi
+      embed = discord.Embed(color=discord.Color.darker_grey(),
+      description=f"What's up everyone! Type `!help` to see all of my commands and get started!\n\n")
+      embed.timestamp = datetime.datetime.utcnow()
+      await channel.send(embed=embed)
+    
     #Welcoming new Members
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -17,12 +31,19 @@ class Welcome(commands.Cog):
             embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
             embed.timestamp = datetime.datetime.utcnow()
             
-            channelnames = ['mem', 'new', 'member', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter', 'general']
-            for channel_const in member.guild.text_channels:
-              for chann in channelnames:
-                if chann in channel_const.name:
-                  channel = channel_const
-                break
+            #channelnames = ['mem', 'new', 'member', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter']
+            #for channel_const in member.guild.text_channels:
+              #for chann in channelnames:
+               # if chann in channel_const.name:
+                 # channel = channel_const
+                  #break {Complicated version of the lambda function, but still works}
+            namelist = ['memb', 'new', 'user', 'User', 'gate', 'enter', 'entr']
+            channel = discord.utils.find(any(map(lambda c: c in c.name, namelist)), member.guild.text_channels) #Checks for a keyword inside a channel name
+            
+            if not channel:
+              newlist = ['gener', 'chat', 'welc']
+              newchann = discord.utils.find(any(map(lambda n: n in n.name, newlist)), member.guild.channels)
+              await newchann.send("Welcome and Goodbye messages to new users won't be sent without the channel including a keyword `new`, `memb` or `user`") #Tries to find a general chat channel to send this error in case the channel doesn't exist
             message = await channel.send(embed=embed)
             await message.add_reaction("🤙🏽")
         except Exception as error:
@@ -37,12 +58,17 @@ class Welcome(commands.Cog):
         embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
         embed.timestamp = datetime.datetime.utcnow()
 
-        channelnames = ['mem', 'new', 'member', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter', 'general']
-        for channel_const in member.guild.text_channels:
-          for chann in channelnames:
-            if chann in channel_const.name:
-              channel = channel_const
-            break
+        channelnames = ['memb', 'new', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter']
+       # for channel_const in member.guild.text_channels:
+         # for chann in channelnames:
+            #if chann in channel_const.name:
+              #channe {Ignore this}
+        channel = discord.utils.find(any(map(lambda c: c in c.name, channelnames)), member.guild.text_channels)
+        
+        if not channel:
+          newlist = ['gener', 'chat', 'welc']
+          newchann = discord.utils.find(any(map(lambda n: n in n.name, newlist)), member.guild.text_channels)
+          await newchann.send("Welcome and Goodbye messages to new users won't be sent without the channel including a keyword `new`, `memb` or `user`") #In case the channel doesn't exist to welcome new people, this will find a different general or chat channel to send the error to
         message = await channel.send(embed=embed)
         await message.add_reaction("👋🏽")
 
