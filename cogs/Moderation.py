1982b9c0fb8d8e1293be76c58efa60e07d240c26
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-29 01:41:36 -0400

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -1,206 +1,110 @@
 import discord
+from typing import Optional
 from discord.ext import commands
-import asyncio
-from asyncio import sleep
+from discord.ext.commands.cooldowns import BucketType
+import platform
 import datetime
+import random
+import asyncio
 
-# This prevents staff members from being punished 
-class Sinner(commands.Converter):
-    async def convert(self, ctx, argument):
-        argument = await commands.MemberConverter().convert(ctx, argument) # gets a member object
-        permission = argument.guild_permissions.manage_messages # can change into any permission
-        if not permission: # checks if user has the permission
-            return argument # returns user object
-        else:
-            raise commands.BadArgument("You cannot punish other staff members") # tells user that target is a staff member
+class General(commands.Cog):
 
-## Converters
+    """{_*General Commands You Can Use*_}"""
 
-def can_execute_action(ctx, user, target):
-    return user.id == ctx.bot.owner_id or \
-           user == ctx.guild.owner or \
-           user.top_role > target.top_role
-
-class MemberNotFound(Exception):
-    pass
-
-async def resolve_member(guild, member_id):
-    member = guild.get_member(member_id)
-    if member is None:
-        if guild.chunked:
-            raise MemberNotFound()
-        try:
-            member = await guild.fetch_member(member_id)
-        except discord.NotFound:
-            raise MemberNotFound() from None
-    return member
-
-class MemberID(commands.Converter):
-    async def convert(self, ctx, argument):
-        try:
-            m = await commands.MemberConverter().convert(ctx, argument)
-        except commands.BadArgument:
-            try:
-                member_id = int(argument, base=10)
-                m = await resolve_member(ctx.guild, member_id)
-            except ValueError:
-                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
-            except MemberNotFound:
-                # hackban case
-                return type('_Hackban', (), {'id': member_id, '__str__': lambda s: f'Member ID {s.id}'})()
-
-        if not can_execute_action(ctx, ctx.author, m):
-            raise commands.BadArgument('You cannot do this action on this user due to role hierarchy.')
-        return m
-
-class BannedMember(commands.Converter):
-    async def convert(self, ctx, argument):
-        if argument.isdigit():
-            member_id = int(argument, base=10)
-            try:
-                return await ctx.guild.fetch_ban(discord.Object(id=member_id))
-            except discord.NotFound:
-                raise commands.BadArgument('This member has not been banned before.') from None
-
-        ban_list = await ctx.guild.bans()
-        entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
-
-        if entity is None:
-            raise commands.BadArgument('This member has not been banned before.')
-        return entity
-
-class ActionReason(commands.Converter):
-    async def convert(self, ctx, argument):
-        ret = f'{ctx.author} (ID: {ctx.author.id}): {argument}'
-
-        if len(ret) > 512:
-            reason_max = 512 - len(ret) + len(argument)
-            raise commands.BadArgument(f'Reason is too long ({len(argument)}/{reason_max})')
-        return ret
-
-def safe_reason_append(base, to_append):
-    appended = base + f'({to_append})'
-    if len(appended) > 512:
-        return base
-    return appended
-
-class Moderation(commands.Cog):
-    """{_*Commands for Moderating the Server*_}"""
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.has_permissions(kick_members = True)
-    async def kick(self, ctx, user : discord.Member):
-        """`Kicks a user from the server`"""
-        if ctx.author == user:
-            await ctx.send("You cannot kick yourself.")
-        else:
-            await user.kick()
-            embed = discord.Embed(title=f'User {user.name} has been kicked.', color=0x00ff00)
-            embed.add_field(name="Tough luck", value="👋🏽")
-            embed.set_thumbnail(url=user.avatar_url)
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.has_permissions(ban_members = True)
-    @commands.guild_only()
-    async def ban(self, ctx, user : discord.Member):
-        """`Bans a user from the server`"""
-        if ctx.author == user:
-            await ctx.send("You cannot ban yourself.")
-        else:
-            await user.ban()
-            embed = discord.Embed(title=f'Banned {user.name}', description=f'{user.mention} has been banned', color=discord.Color.dark_red())
-            embed.set_thumbnail(url=user.avatar_url)
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, *, member):
-        """`Unbans a member from the server (!unban Example name#1234)`"""
-        banned_users = await ctx.guild.bans()
-        member_name, member_discriminator = member.split('#')
-        
-        for ban_entry in banned_users:
-            user = ban_entry.user
             
-            if (user.name, user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(user)
-                embed = discord.Embed(title=f'Unbanned {user.name}', description=f'{user.mention} has been unbanned', color=discord.Color.dark_green())
-                embed.set_thumbnail(url=user.avatar_url)
-                await ctx.send(embed=embed)
-
     @commands.command()
     @commands.guild_only()
-    @commands.has_any_role('Moderator', 'Executive Admin')
-    async def mute(self, ctx, user : discord.Member, time: int):
-        """`Prevents a user from speaking for a specified amount of time`"""
-        if ctx.author == user:
-            await ctx.send("You cannot mute yourself.")
-        else:
-            rolem = discord.utils.get(ctx.message.guild.roles, name='Muted')
-            dick = discord.utils.get(ctx.message.guild.roles, name = 'Verified Member')
-            if rolem is None:
-                embed=discord.Embed(title="Muted role", url="http://echo-bot.wikia.com/wiki/Setting_up_the_muted_role", description="The mute command requires a role named 'Muted'.", color=discord.Color.dark_red())
-                embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar_url)
-                embed.set_footer(text="Without this role, the command will not work.")
-                await ctx.send(embed=embed)
-            elif rolem not in user.roles:
-                embed = discord.Embed(title=f'User {user.name} has been successfully muted for {time}s.', color=discord.Color.dark_red())
-                embed.add_field(name="Shhh!", value=":zipper_mouth:")
-                embed.set_thumbnail(url=user.avatar_url)
-                await ctx.send(embed=embed)
-                await user.add_roles(rolem)
-                await user.remove_roles(dick)
-                await sleep(time)
-                if rolem in user.roles:
-                    try:
-                        await user.remove_roles(rolem)
-                        await user.add_roles(dick)
-                        embed = discord.Embed(title=f'User {user.name} has been automatically unmuted.', color=discord.Color.dark_green())
-                        embed.add_field(name="Welcome back!", value=":open_mouth:")
-                        embed.set_thumbnail(url=user.avatar_url)
-                        await ctx.send(embed=embed)
-                    except Exception:
-                        print(f'User {user.name} could not be unmuted!')
-            else:
-                await ctx.send(f'User {user.mention} is already muted.')
-
-    @commands.command()
-    @commands.guild_only()
-    @commands.has_any_role('Moderator', 'Executive Admin')
-    async def unmute(self, ctx, user: discord.Member):
-        """`Unmutes a user`"""
-        rolem = discord.utils.get(ctx.message.guild.roles, name='Muted')
-        dick = discord.utils.get(ctx.message.guild.roles, name = 'Verified Member')
-        if rolem in user.roles:
-            embed = discord.Embed(title=f'User {user.name} has been manually unmuted.', color=discord.Color.dark_green())
-            embed.add_field(name="Welcome back!", value=":open_mouth:")
-            embed.set_thumbnail(url=user.avatar_url)
-            await ctx.send(embed=embed)
-            await user.remove_roles(rolem)
-            await user.add_roles(dick)
-
-    @commands.command(aliases=['prune', 'clean'])
-    @commands.guild_only()
-    @commands.has_permissions(manage_messages = True)
-    async def purge(self, ctx, count: int):
-        """`Deletes a specified amount of messages. (Max 100)`"""
-        await ctx.message.delete()
-        if not count:
-            await ctx.send("Include the amount of messages to delete, you dummy", delete_after=3)
-            return
+    @commands.cooldown(1 , 30, type=BucketType.channel)
+    async def binfo(self, ctx):
+        """
+        `Shows info about the bot`
+        """
         
-        if count>100:
-            count = 1
-        await ctx.message.channel.purge(limit=count, bulk=True)
+        pythonVersion = platform.python_version()
+        dpyVersion = discord.__version__
+        serverCount = len(self.bot.guilds)
+        embed = discord.Embed(title='My Info', color=discord.Color.dark_red())
+        embed.add_field(name='My Owner:', value="Andrew Nunnes#1148", inline=True)
+        embed.add_field(name='Python Version:', value=f"I'm running version {pythonVersion} of Python", inline=True)
+        embed.add_field(name='Discord Version:', value=f"I'm running Discord Version {dpyVersion}", inline=True)
+        embed.add_field(name='Server Count:', value=f"I'm in {serverCount} server(s)", inline=True)
+        
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(1, 30, type=BucketType.channel)
+    async def sinfo(self, ctx):
+        """
+        `Shows the stats/info of the server`
+        """
+
+        memberCount = len(set(self.bot.get_all_members()))
+        poo = len(list(filter(lambda m: not m.bot, ctx.guild.members)))
+        pee = len(list(filter(lambda m: m.bot, ctx.guild.members)))
+
+        statuses = [len(list(filter(lambda m: str(m.status) == "online" , ctx.guild.members))),
+                    len(list(filter(lambda m: str(m.status) == "idle" , ctx.guild.members))),
+                    len(list(filter(lambda m: str(m.status) == "dnd" , ctx.guild.members))),
+                    len(list(filter(lambda m: str(m.status) == "offline" , ctx.guild.members)))]
+
+        embed = discord.Embed(title=f'My Stats/Server Info', color=0x000000)
+        embed.set_footer(text=f'Created · {ctx.guild.created_at.strftime("%d/%m/%Y")}')
+        embed.set_author(name=f"Command requested by: {ctx.author}", icon_url=f"{ctx.author.avatar_url}")
+        embed.add_field(name='Name:', value=ctx.guild.name, inline=True)
+        embed.add_field(name='Region:', value=ctx.guild.region, inline=True)
+        embed.add_field(name='Owner:', value=ctx.guild.owner, inline=True)
+        embed.add_field(name='Member Count:', value=f'<:online:728377717090680864>{statuses[0]}, <:idle:728377738599071755>{statuses[1]}, <:dnd:728377763458973706>{statuses[2]}, <:offline:728377784207933550>{statuses[3]}\nTotal: {memberCount}', inline=True)
+        embed.add_field(name='Banned Members:', value=f'{len(await ctx.guild.bans())}', inline=True)
+        embed.add_field(name='Humans:', value=f'{poo} Humans', inline=True)
+        embed.add_field(name='Bots:', value=f'{pee} Bots', inline=True)
+        embed.add_field(name='Channels:', value=f'<:textch:728377808518381679>{len(ctx.guild.text_channels)}\n<:voicech:728377834187259976>{len(ctx.guild.voice_channels)}', inline=True)
+        embed.add_field(name='Invites:', value=f'{len(await ctx.guild.invites())}', inline=True)
+        await ctx.channel.send(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(3, 15, type=BucketType.user)
+    async def ping(self, ctx):
+        """
+        `Runs a connection test to discord`
+        """
+        embed = discord.Embed(title='Pong?', color=discord.Color.dark_blue())
+        await ctx.send(embed=embed, delete_after=0.3)
             
         await asyncio.sleep(0.5)
+            
+        embed = discord.Embed(title='My Connection:', description=f'__**My ping is {round(self.bot.latency * 1000)}ms**__', color=discord.Color.dark_blue())
+        await ctx.send(embed=embed)
 
-        await ctx.send(f"{count} message(s) have been deleted 🗑", delete_after=2)
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(3, 20, type=BucketType.user)
+    async def uinfo(self, ctx, member: discord.Member = None):
+        """
+        `Shows info about you`
+        """
+
+        member = ctx.author if not member else member
+        roles = [role for role in member.roles]
+
+        embed = discord.Embed(title=f"{{{member.name}'s General Info}}", description=f"**>Discord Tag: **{member}\n**>Nick: **{member.nick}\n**>User ID: **{member.id}\n**>Created: **{member.created_at.strftime('%a %#d %B %Y, %I:%M %p')}\n**>Joined: **{member.joined_at.strftime('%a %#d %B %Y, %I:M %p')}\n**>Roles {{{len(roles)}}}': **{' '.join([role.mention for role in roles])}\n**>Top Role: **{member.top_role.mention}", colour=0x000000)
+        embed.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    @commands.guild_only()
+    @commands.cooldown(3, 20, type=BucketType.user)
+    async def avatar(self, ctx, member: discord.Member = None):
+        """`Get an enlarged version of somebody's avatar`"""
+        member = ctx.author if not member else member
+        userinfo = member
+        embed = discord.Embed(title=f"{userinfo.name}'s Avatar", color=0x000000, timestamp = datetime.datetime.utcnow())
+        embed.set_author(name=f"Command requested by: {ctx.message.author}")
+        embed.set_image(url=userinfo.avatar_url_as(format='png'))
+        await ctx.send(embed=embed)
 
 def setup(bot):
-    bot.add_cog(Moderation(bot))
+    bot.add_cog(General(bot))
