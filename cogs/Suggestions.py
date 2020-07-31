26aaf34f6eb4dbac2cfbe182ba8dacaf1e1d21eb
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-31 16:33:50 -0400

    Update Suggestions.py

--- cogs/Suggestions.py
+++ cogs/Suggestions.py
@@ -19,13 +19,28 @@ class Suggestions(commands.Cog):
         """
         await ctx.message.delete()
         try:
-            embed = discord.Embed(description=f"__*Suggestion provided by {ctx.author.mention}*__: {sug}\n\nReact down below to leave your opinion! ⬇️", color=discord.Color.dark_purple())
+            embed = discord.Embed(description=f"__*Suggestion provided by {ctx.author.mention}*__: {sug}\n\nReact down below to leave your opinion! ⬇️\n\n<:greenmark:738415677827973152> {{Yes}}\n<:redmark:738415723172462723> {{No}}\n<:maybemark:738418156808175616> {{Maybe}}", color=discord.Color.dark_purple())
             embed.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
             embed.timestamp = datetime.datetime.utcnow()
-            channel = ctx.guild.get_channel(726689140862746664)
+            #channelnames = ['suggest', 'suggestions']
+            #for channel_const in ctx.guild.text_channels:
+              #for chann in channelnames:
+                #if chann in channel_const.name:
+                  #channel = channel_const
+                 # break
+                #if not chann:
+                 # await ctx.send("I can't seem to find a suggestion channel")
+            
+            channel = discord.utils.find(lambda g: 'sugg' in g.name, ctx.guild.text_channels)
+            
+            if not channel:
+              await ctx.send("I can't seem to find a Suggestion channel")
+                 
             poo = await channel.send(embed=embed)
-            await poo.add_reaction("☑️")
-            await poo.add_reaction("🚫")
+            await poo.add_reaction("<:greenmark:738415677827973152>")
+            await poo.add_reaction("<:redmark:738415723172462723>")
+            await poo.add_reaction("<:maybemark:738418156808175616>")
+            
         except Exception as error:
             raise(error)
 
