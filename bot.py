a40ec869429aeb9f2992be5ab0f350ac5ddde321
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-07-31 16:38:11 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,71 @@ from discord.ext import commands
 import pathlib
 import logging
 import platform
+import json
+from pathlib import Path
+import datetime
+import os
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or('!'), case_insensitive=True)
+import cogs._json
+
+cwd = Path(__file__).parents[0]
+cwd = str(cwd)
+print(f"{cwd}\n•—•—•—•—•—•—•—•—•—•")
+
+#Function for getting the prefix data from a json file
+def get_prefix(client, message):
+  data = cogs._json.read_json('prefixes')
+  if not str(message.guild.id) in data:
+    return commands.when_mentioned_or('!')(client, message)
+  return commands.when_mentioned_or(data[str(message.guild.id)])(client, message)
+  
+def file_name(file):
+    if file.endswith(".py") and not file.startswith("_"):
+        client.load_extension(f"cogs.{file[:-3]}")
+
+client = commands.Bot(command_prefix=get_prefix, case_insensitive=True)
 client.remove_command('help')
 
+client.cwd = cwd 
+
+@client.event
+async def on_message(message):
+  #When the bot is mentioned, it'll respond with this embed
+  if f"<@!{client.user.id}>" in message.content:
+    data = cogs._json.read_json('prefixes')
+    if str(message.guild.id) in data:
+        prefix = data[str(message.guild.id)]
+    else:
+        prefix = '!'
+    prefixembed = discord.Embed(
+      description=f"What's up {message.author.mention}. My prefix is `{prefix}`\nFeel free to change it with `!prefix <newprefix>`", 
+      color=discord.Color.darker_grey())
+    
+    prefixembed.timestamp = datetime.datetime.utcnow()
+  
+    await message.channel.send(embed=prefixembed)
+    
+  await client.process_commands(message)
+  
 @client.event
 async def on_ready():
     print("Bot is working")
     return await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Flight take another L"))
     
+if __name__ == '__main__':
+    for file in os.listdir(cwd+"/cogs/"):
+        file_name(file)
+    for file in os.listdir(cwd+"/cogs/events/"):
+        file_name(f'events.{file}')
+    for file in os.listdir(cwd+"/cogs/other/"):
+        file_name('other.{file}')
+                #if file.endswith(".py") and not file.startswith("_"):
+                    #client.load_extension(f"cogs.{file[:-3]}")
+    
 #for ext in [file.stem for file in pathlib.Path('cogs').glob('**/*.py')]:
     #client.load_extension(f"cogs.{ext}")
     
-for ext in[".".join(p.parts)[:-len(".py")] for p in pathlib.Path('cogs').glob('**/*.py')]:
-  client.load_extension(ext)
+#for ext in[".".join(p.parts)[:-len(".py")] for p in pathlib.Path('cogs').glob('**/*.py')]:
+  #client.load_extension(ext)
     
 client.run("bruh")
