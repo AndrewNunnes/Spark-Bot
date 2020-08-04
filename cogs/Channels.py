8f516863084382a589db5dd36cd57b3b74431998
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-04 18:51:47 -0400

    Update Channels.py

--- cogs/Channels.py
+++ cogs/Channels.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from typing import Optional
+import typing
+import asyncio
+import datetime
 
 class Channels(commands.Cog):
   
@@ -14,7 +17,7 @@ class Channels(commands.Cog):
     usage="textchannel <name> (category) (slowmode_delay[in seconds]) (reason)")
   @commands.guild_only()
   @commands.has_permissions(manage_channels=True)
-  async def textchannel(self, ctx, name, *, category: discord.CategoryChannel=None, slowmode: int=None, reason=None):
+  async def textchannel(self, ctx, name, category: discord.CategoryChannel=None, slowmode: int=None, *, reason=None):
     
     guild = ctx.guild
     
@@ -30,6 +33,13 @@ class Channels(commands.Cog):
     
     await ctx.send(embed=e)
     
+    if len(name) > 25:
+      await ctx.send("New name can't be longer than 25 letters")
+      
+    else:
+      if len(name) < 2:
+        await ctx.send("New name must be at least 2 letters long")
+    
   @textchannel.error
   async def textchannel_error(self, ctx, error):
     if isinstance(error, commands.BadArgument):
@@ -44,7 +54,7 @@ class Channels(commands.Cog):
     usage="voicechannel <name> (category) (bitrate[number]) (user_limit[number]) (reason)")
   @commands.guild_only()
   @commands.has_permissions(manage_channels=True)
-  async def voicechannel(self, ctx, name, *, category: discord.CategoryChannel=None, bitrate: int=None, user_limit: int=None, reason=None):
+  async def voicechannel(self, ctx, name, category: discord.CategoryChannel=None, bitrate: int=None, user_limit: int=None, *, reason=None):
     
     guild = ctx.guild
     
@@ -63,6 +73,12 @@ class Channels(commands.Cog):
     
     await ctx.send(embed=e)
     
+    if len(name) > 25:
+      await ctx.send("New name can't be longer than 25 letters")
+    else:
+      if len(name) < 2:
+        await ctx.send("New name must be at least 2 letters long")
+    
   @voicechannel.error
   async def voicechannel_error(self, ctx, error):
     if isinstance(error, commands.BadArgument):
@@ -77,8 +93,64 @@ class Channels(commands.Cog):
     usage="delete <channel>")
   @commands.guild_only()
   @commands.has_permissions(manage_channels=True)
-  async def deletechannel(self, ctx):
-    pass
+  async def deletechannel(self, ctx, channel: typing.Union[discord.TextChannel, discord.VoiceChannel], *, reason=None):
+    
+    guild = ctx.guild
+    
+    await channel.delete(reason=reason)
+    
+    await asyncio.sleep(0.5)
+    
+    await ctx.send(f"Yo {ctx.author.mention}, I just deleted {channel} for you")
+    
+  @commands.command(
+    brief="{Info on a Channel}", 
+    usage="chinfo <#channel>")
+  @commands.guild_only()
+  async def chinfo(self, ctx, channel: typing.Union[discord.TextChannel, discord.VoiceChannel]):
+    
+    #bitrate = discord.VoiceChannel.bitrate
+    
+    if isinstance(channel, discord.VoiceChannel):
+      
+      chtype = discord.ChannelType
+      
+      e = discord.Embed(
+        title=f"Info for {channel} `Voice Channel`",
+        description=f"**>Name:** {channel.name}\n**>ID:** {channel.id}\n**>Position**: {channel.position}\n**>Bit Rate:** {channel.bitrate}\n**>User Limit:** {channel.user_limit}\n**>Members in Channel:** {len(channel.members)}\n**>Created At:** {channel.created_at}", 
+        color=discord.Color.darker_grey())
+        
+      #await ctx.send(embed=e)
+      
+    elif isinstance(channel, discord.TextChannel):
+      
+      e = discord.Embed(
+        title=f"Info for {channel} `Text Channel`", 
+        description=f"**>Name:** {channel.name}\n**>ID:** {channel.id}\n**>Topic:** {channel.topic}\n**>Position:** {channel.position}\n**>Slowmode Delay:** {channel.slowmode_delay}\n**>NSFW?** {channel.is_nsfw()}\n**>News Channel?** {channel.is_news()}\n**>Under Category:** {channel.category}\n**>Created At:** {channel.created_at}", 
+        color=discord.Color.darker_grey())
+        
+      e.set_author(
+        name=f"{ctx.author}", 
+        icon_url=ctx.author.avatar_url)
+        
+      #await ctx.send(embed=e)
+        
+    else:
+      
+      e = discord.Embed(
+        description="_*• That Channel doesn't exist*_", 
+        color=0x420000)
+        
+    await ctx.send(embed=e)
+    
+  @chinfo.error
+  async def chinfo_error(self, ctx, error):
+    if isinstance(error, commands.BadUnionArgument):
+      e = discord.Embed(
+        description='Either:\n• When specifying a Voice Channel, only use the name of it\nExample: `!chinfo "Voice Channel"`\n• That Channel does not exist', 
+        color=0x420000)
+        
+      await ctx.send(embed=e)
  
   @commands.command(
     brief="{Get a List of Channels in the Server}", 
@@ -87,6 +159,34 @@ class Channels(commands.Cog):
   #@commands.has_permissions(manage_channels=True)
   async def channels(self, ctx):
     pass
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 def setup(bot):
   bot.add_cog(Channels(bot))
