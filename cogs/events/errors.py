034e238db99df3c7480c15e415e92309ea26ba16
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-04 18:53:36 -0400

    Update errors.py

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -1,37 +1,94 @@
 import discord
 from discord.ext import commands
+import datetime
+import asyncio
+import re
 
-class Errors(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class Logging(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.Cog.listener()
+  async def on_message(self, message):
+    
+    #Check if a user
+    #send a Discord Invite
+    if "discord.gg" in message.content:
+      guild = message.guild
+      member = message.author
+      
+      await message.delete()
+      
+      bruh = discord.Embed(
+        title=f"__*{message.author.mention}*__ you're not supposed to share Server Invites", 
+        color=0x420000)
+      
+      bruh.set_thumbnail(url=message.author.avatar_url)
+      bruh.set_footer(text=f"{message.author.name} Tried to Post an Invite")
+      bruh.timestamp = datetime.datetime.utcnow()
+      
+      await message.channel.send(embed=bruh)
+      
+      await asyncio.sleep(1)
+      
+      names = ['log']
+      #Checks if there's a channel 
+      #containing log in the name
+      channel = discord.utils.find(
+        lambda channel:any(
+          map(lambda c: c in channel.name, names)), 
+          guild.text_channels)
+      
+      e = discord.Embed(
+        title="__*Invite Attempt*__", 
+        description=f"_**{message.author.name.discriminator} Tried to post an invite to a Discord Server\nShould let him know not to do it next time**_", 
+        color=0x380000)
+      
+      e.set_thumbnail(url=message.author.avatar_url)
+      e.set_footer(text=f"{message.author}")
+      e.timestamp = datetime.datetime.utcnow()
+      
+      await channel.send(embed=e)
+      
+      #If a log channel doesn't exist
+      #This will send
+      if not channel:
+        await message.channel.send("Looks like you don't have a Logs channel\n\nMaking one now...")
+        
+        guild = message.guild
+        
+        ow = {
+          guild.default_role: discord.PermissionOverwrite(read_messages=False)
+        }
+        for role in guild.roles:
+          if role.permissions.kick_members:
+            ow[role] = discord.PermissionOverwrite(read_messages=True)
+        
+        await guild.create_text_channel("⚠️ Server Logs", overwrites=ow, reason="Logging for Moderation")
 
-    #All global errors
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        error = getattr(error, 'original', error)
-        if isinstance(error, commands.CommandNotFound):
-            await ctx.send('~~Command doesn\'t exist bro~~')
-            await ctx.message.add_reaction('⛔')
-        elif isinstance(error, commands.NotOwner):
-          embed = discord.Embed(description="You're not the Owner of this bot dummy", color=discord.Color.dark_red)
-          await ctx.send(embed=embed)
-        elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.send('~~You can\'t use commands outside of the server~~')
-        elif isinstance(error, commands.CommandOnCooldown):
-            guild = ctx.guild
-            if not guild:
-                await ctx.reinvoke()
-            else:
-                var1 = error.retry_after
-                var2 = int(var1)
-                await ctx.send(f'You can\'t use this command for another {var2} seconds', delete_after=5)
-        elif isinstance(error, commands.MissingPermissions):
-            poo = discord.Embed(description="You don't have the permissions to do that, idiot", color=discord.Color.dark_red())
-            await ctx.send(embed=poo, delete_after=5)
-        elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Seems like you're forgetting to put something bruh", delete_after=5)
-        else:
-            raise(error)
-
+    #await self.bot.process_commands(message)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 def setup(bot):
-    bot.add_cog(Errors(bot))
+  bot.add_cog(Logging(bot))
