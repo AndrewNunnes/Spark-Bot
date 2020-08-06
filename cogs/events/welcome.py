77610ef85039626bbcb01a3c6a3b5f3dc48f4fb3
AndrewNunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-05 22:00:58 -0400

    Update welcome.py

--- cogs/events/welcome.py
+++ cogs/events/welcome.py
@@ -2,93 +2,96 @@ import discord
 from discord.ext import commands
 import datetime
 import asyncio
-import re
 
-class Logging(commands.Cog):
-  def __init__(self, bot):
-    self.bot = bot
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
     
-  @commands.Cog.listener()
-  async def on_message(self, message):
-    
-    #Check if a user
-    #send a Discord Invite
-    if "discord.gg" in message.content:
-      guild = message.guild
-      member = message.author
-      
-      await message.delete()
-      
-      bruh = discord.Embed(
-        title=f"__*{message.author.mention}*__ you're not supposed to share Server Invites", 
-        color=0x420000)
-      
-      bruh.set_thumbnail(url=message.author.avatar_url)
-      bruh.set_footer(text=f"{message.author.name} Tried to Post an Invite")
-      bruh.timestamp = datetime.datetime.utcnow()
-      
-      await message.channel.send(embed=bruh)
-      
-      await asyncio.sleep(1)
-      
-      names = ['log']
-      #Checks if there's a channel 
-      #containing log in the name
+    #Sends a fancy embed to show the prefixes    
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+      names = ['comman', 'bot']
       channel = discord.utils.find(
         lambda channel:any(
-          map(lambda c: c in channel.name, names)), 
-          guild.text_channels)
-      
-      e = discord.Embed(
-        title="__*Invite Attempt*__", 
-        description=f"_**{message.author.name.discriminator} Tried to post an invite to a Discord Server\nShould let him know not to do it next time**_", 
-        color=0x380000)
-      
-      e.set_thumbnail(url=message.author.avatar_url)
-      e.set_footer(text=f"{message.author}")
-      e.timestamp = datetime.datetime.utcnow()
-      
-      await channel.send(embed=e)
-      
-      #If a log channel doesn't exist
-      #This will send
-      if not channel:
-        await message.channel.send("Looks like you don't have a Logs channel\n\nMaking one now...")
+          map(lambda w: w in channel.name, names)),
+        guild.text_channels) #When the bot joins a server, this will check for a bot commands channel to send an embed
         
-        guild = message.guild
-        
-        ow = {
-          guild.default_role: discord.PermissionOverwrite(read_messages=False)
-        }
-        for role in guild.roles:
-          if role.permissions.kick_members:
-            ow[role] = discord.PermissionOverwrite(read_messages=True)
-        
-        await guild.create_text_channel("⚠️ Server Logs", overwrites=ow, reason="Logging for Moderation")
+      if not channel: #If a bot commands channel doesn't exist, it'll look for a general channel
+        newchann = ['gener', 'chat', 'welc', 'memb']
+        new = discord.utils.find(
+          lambda new:any(
+            map(lambda n: n in new.name, newchann)),
+            guild.text_channels)
+            
+        embed = discord.Embed(
+          color=discord.Color.darker_grey(), 
+          description=f"What's up everyone! Type in `!help` to see all of my available commands and get started!\n\n")
+        embed.set_footer(text="Remember to make my Role higher than everyone else for certain commands to work properly")
+        embed.timestamp = datetime.datetime.utcnow()
+        await new.send(embed=embed)
+            
+      embed = discord.Embed(color=discord.Color.darker_grey(),
+      description=f"What's up everyone! Type `!help` to see all of my commands and get started!\n\n")
+      
+      embed.timestamp = datetime.datetime.utcnow()
+      await channel.send(embed=embed)
+      
+      #await guild.create_custom_emoji()
+    
+    #Welcoming new Members
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            embed = discord.Embed(colour=discord.Colour.dark_green(), description=f"What's up {member}, and welcome to the server! You are now member {len(list(member.guild.members))}")
+            embed.set_thumbnail(url=f"{member.avatar_url}")
+            embed.set_author(name=f"Welcome {member.name}", icon_url=f"{member.avatar_url}")
+            embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+            embed.timestamp = datetime.datetime.utcnow()
+            
+            #channelnames = ['mem', 'new', 'member', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter']
+            #for channel_const in member.guild.text_channels:
+              #for chann in channelnames:
+               # if chann in channel_const.name:
+                 # channel = channel_const
+                  #break {Complicated version of the lambda function, but still works}
+            namelist = ['memb', 'new', 'user', 'User', 'gate', 'enter', 'entr']
+            channel = discord.utils.find(
+              lambda channel:any(
+                map(lambda c: c in channel.name, namelist)), member.guild.text_channels) #Checks for a keyword inside a channel name
+            
+            if not channel:
+              newlist = ['gener', 'chat', 'welc']
+              newchann = discord.utils.find(
+                lambda newchann:any(
+                  map(lambda n: n in newchann.name, newlist)), member.guild.channels)
+              await newchann.send("Welcome and Goodbye messages to new users won't be sent without the channel including a keyword `new`, `memb` or `user`") #Tries to find a general chat channel to send this error in case the channel doesn't exist
+            message = await channel.send(embed=embed)
+            await message.add_reaction("🤙🏽")
+        except Exception as error:
+            raise (error)
 
-    #await self.bot.process_commands(message)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+    #Saying goodbye to leaving members
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        embed = discord.Embed(colour=discord.Colour.dark_red(), description=f"{member} just left the server. Thanks for visiting! Member Count: {len(list(member.guild.members))}")
+        embed.set_thumbnail(url=f"{member.avatar_url}")
+        embed.set_author(name=f"Goodbye {member.name}", icon_url=f"{member.avatar_url}")
+        embed.set_footer(text=f"{member.guild}", icon_url=f"{member.guild.icon_url}")
+        embed.timestamp = datetime.datetime.utcnow()
+
+        channelnames = ['memb', 'new', 'user', 'User', 'gateway', 'gate', 'entrance', 'enter', 'leave']
+        channel = discord.utils.find(
+            lambda channel:any(
+                map(lambda c: c in channel.name, channelnames)), member.guild.text_channels)
+        
+        if not channel:
+          newlist = ['gener', 'chat', 'welc']
+          newchann = discord.utils.find(
+              lambda newchann:any(
+                  map(lambda n: n in newchann.name, newlist)), member.guild.text_channels)
+          await newchann.send("Welcome and Goodbye messages to new users won't be sent without the channel including a keyword `new`, `memb` or `user`") #In case the channel doesn't exist to welcome new people, this will find a different general or chat channel to send the error to
+        message = await channel.send(embed=embed)
+        await message.add_reaction("👋🏽")
+
 def setup(bot):
-  bot.add_cog(Logging(bot))
+    bot.add_cog(Welcome(bot))
