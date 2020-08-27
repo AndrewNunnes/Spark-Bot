129d89ec6434fbe2970234a4c3ff966ed2150d1d
Nunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-27 00:50:42 -0400

    Moved Commands into Misc Category

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,92 +1,135 @@
+#•----------Modules----------•#
+
 import discord
-from discord.ext import commands
+
 import asyncio
+
 import random
+
 from datetime import datetime
 
-#import datetime
-#import textwrap
-#import traceback
+from discord.ext.commands import BucketType, command, bot_has_permissions, guild_only, Cog, cooldown, has_permissions
 
-#import lightbulb
-#import hikari
-#from hikari.models.intents import Intent
+from typing import Optional
 
-def to_emoji(c):
-    base = 0x1f1e6
-    return chr(base + c)
+#•----------Class----------•#
 
-class Misc(commands.Cog):
+class Misc(Cog, name="Misc Category"):
 
-    """🔗 `{Miscallaneous Commands}`"""
+    """`{Miscallaneous Commands}`"""
 
     def __init__(self, bot):
         self.bot = bot
         
-    @commands.command(
-      brief="{Create a Pernament Invite for a Channel}", 
-      usage="createinvite <#channel>")
-    @commands.has_permissions(create_instant_invite=True)
-    @commands.guild_only()
-    async def createinvite(self, ctx, channel: discord.TextChannel):
-        """
-        `Creates an Invite for a Specified Channel`
-        """
-        invite = await channel.create_invite()
-        await ctx.send(invite)
-        
-    #@invite.error
-    #async def invite_error(self, ctx, error):
-      #  if isinstance(error, commands.NotOwner):
-          #  embed = discord.Embed(description#="You can't use this command!", color=discord.Color.dark_red())
-          #  embed.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-          #  await ctx.channel.send(embed=e
-    @commands.command(
-      brief="{Bot will announce your message}", 
-      usage="announce (channel) <message_here>")
-    #@commands.has_permissions(kick_members=True)
-    @commands.guild_only()
-    async def announce(self, ctx, channel: discord.TextChannel=None, *, arg):
-      
-        if channel is None:
-            await ctx.send(arg)
-        
-        if channel is not None:
-            await channel.send(arg)
+#•----------Commands----------•#
 
-   # @announce.error
-  #  async def announce_error(self, ctx, error):
-   #     if isinstance(error, commands.MissingRequiredArgument):
-         #   embed = discord.Embed(description=f'Please include the message\n```!announce >message<```', color=discord.Color.dark_red())
-        #    embed.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-          #  await ctx.channel.send(embed=embed, delete_after=5)
-      #  elif isinstance(error, commands.NotOwner):
-         #   embed = discord.Embed(description=f"You can't use this command!", color=discord.Color.dark_red())
-         #   embed.set_author(name=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-         #   await ctx.channel.send(embed=embed, delete_after=5)
-            
-    @commands.command(
-      brief="{DM a User}", 
-      usage="dm <user> <message>")
-    @commands.has_permissions(manage_messages=True)
-    @commands.guild_only()
-    async def dm(self, ctx, user:discord.User, *, content):
-        
-        await user.send(content)
-        
-        await ctx.send(f"Successfully sent {user} a dm")
-            
-    @commands.command(
+    #Booster Command  
+    @command(
+      brief="{List of Boosters for the Server}", 
+      usage="boosters")
+    @guild_only()
+    @cooldown(1, 1.5, BucketType.user)
+    @bot_has_permissions(use_external_emojis=True)
+    async def boosters(self, ctx):
+
+        #Saving the guild's boosters
+        #As a variable
+        booster_list = ctx.guild.premium_subscribers
+
+        #Checking to see if there is no boosters
+        if booster_list == []:
+            boosters = "No Boosters"
+        else:
+            boosters = booster_list
+
+        e = discord.Embed(
+            title=f"<:booster:741407205575622696> __*List of Boosters for {{{ctx.guild.name}}}*__", 
+            description=f"{boosters}", 
+            color=0x420000)
+
+        e.add_field(
+            name=f"__*Total*__", 
+            value=f'{len(booster_list)} Boosters')
+      
+        e.timestamp = datetime.utcnow()
+      
+        await ctx.send(embed=e)
+
+    @command(
+        brief="{Leave a Suggestion}", 
+        usage="suggest <suggestion>", 
+        aliases=['sugg'])
+    @guild_only()
+    @cooldown(1, 1.5, BucketType.user)
+    async def suggest(self, ctx, *, sug):
+      
+        #If the suggestion is too long
+        if len(sug) > 850:
+            e = discord.Embed(
+                description="<:redmark:738415723172462723> __*Suggestion can't be longer than 850 Characters*__", 
+                color=0x420000)
+            await ctx.send(embed=e)
+            return
+      
+        #Look for a keyword 'sugg' in a channel
+        channel = discord.utils.find(lambda g: 'sugg' in g.name, ctx.guild.text_channels)
+           
+        #If there isn't a suggestion channel
+        if not channel:
+            await ctx.send("I can't seem to find a Suggestion channel")
+            return
+       
+        #Make embed
+        e = discord.Embed(
+            color=0x420000)
+          
+        e.set_thumbnail(
+            url=ctx.author.avatar_url)
+      
+        e.set_footer(
+            text=f"Taken by {ctx.author}")
+          
+        e.timestamp = datetime.utcnow()
+      
+        #Delete author's message
+        await ctx.message.delete()
+      
+        #Make the embed fields
+        fields = [
+                  ("**New Suggestion!**", 
+                  sug, False), 
+                
+                  ("**React to Leave your Opinion!**", 
+                  "<:greenmark:738415677827973152> - __*Good*__" +
+                  "\n<:redmark:738415723172462723> - __*Bad*__" +
+                  "\n<:maybemark:738418156808175616> - __*Maybe/Ok*__", True)]
+      
+        #Add fields
+        for n, v, i in fields:
+            e.add_field(
+                name=n, 
+                value=v, 
+                inline=i)
+      
+        #Send the embed
+        m = await channel.send(embed=e)
+      
+        #List the reactions   
+        reactions = ["<:greenmark:738415677827973152>", "<:redmark:738415723172462723>", "<:maybemark:738418156808175616>"]
+      
+        #Add reactions
+        for react in reactions:
+            await m.add_reaction(react)
+
+    @command(
       brief="{Bot Interactively Starts a Poll}", 
       usage="poll")
-    @commands.guild_only()
-    @commands.has_permissions(manage_messages=True)
+    @guild_only()
+    @cooldown(1, 3, BucketType.user)
+    @has_permissions(manage_messages=True)
     async def poll(self, ctx, *, question):
-        """Interactively creates a poll with the following question.
-        To vote, use reactions!
-        """
 
-        # a list of messages to delete when we're all done
+        #A list of messages to delete when we're all done
         messages = [ctx.message]
         answers = []
 
@@ -123,12 +166,15 @@ class Misc(commands.Cog):
     async def poll_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.send('Missing the question.', delete_after=4)
+        else:
+            raise(error)
 
-    @commands.command(
+    @command(
       brief="{Start a poll quickly}", 
       usage="quickpoll <at_least_2_questions>")
-    @commands.guild_only()
-    #@commands.has_permissions(manage_messages=True)
+    @cooldown(1, 3, BucketType.user)
+    @guild_only()
+    @has_permissions(manage_messages=True)
     async def quickpoll(self, ctx, *questions_and_choices: str):
         """Makes a poll quickly.
         The first argument is the question and the rest are the choices.
@@ -161,52 +207,176 @@ class Misc(commands.Cog):
         for emoji, _ in choices:
             await poll.add_reaction(emoji)
             
-    @commands.command(
-      brief="{Get a List of Guilds the Bot's in}", 
-      usage="bguilds", 
-      aliases=['botguilds'])
-    @commands.guild_only()
-    @commands.is_owner()
-    async def bguilds(self, ctx):
+    @command(
+        brief="{Apply for Moderator}", 
+        usage="applymod")
+    @cooldown(1, 3, BucketType.user)
+    @guild_only()
+    async def applymod(self, ctx, member: discord.Member = None):
 
-        #Empty String 
-        #To add guild names later
-        g_list = ''
+        member = member or ctx.author
+
+        def checkreact(reaction, user):
+            return user.id == member.id and str(reaction.emoji) in ["✅", "❌"]
+
+        applicationQuestions = [
+            "What's your Minecraft IGN + Discord Username?",
+            "How old are you? (If you feel uncomfortable saying this, just confirm if you're at least a teenager)",
+            "What Time Zone do you live in? (So I know when you're online, and gives me a reason if you're not too active)",
+            "Why do you want to be Moderator? Isn't it fun to play without any responsibilites?",
+            "What will you do for the Discord Server?",
+            "Anything else you want to say?",
+        ]
         
-        #Iterate through servers bot's in
-        for g in self.bot.guilds:
-            #Add the guild names
-            #To the empty string
-            g_list += f'• {"".join(g.name)}\n'
+        await ctx.send("__*Application will be sent to you soon...*__")
         
-        #Make embed
-        e = discord.Embed(
-            title=f"__*Total Guilds {{{len(self.bot.guilds)}}}*__", 
-            description=g_list)
-        
-        e.timestamp = datetime.utcnow()
-        
-        e.set_footer(
-            text=ctx.author, 
-            icon_url=ctx.author.avatar_url)
-        
-        await ctx.send(embed=e)
+        applicationAnswers = {}
+        for question in applicationQuestions:
+            answer = await GetMessage(self.bot, member=member, contentOne=question, timeout=500)
             
-    @commands.command(
-      brief="{Restart the Bot} [BOT OWNER ONLY]", 
-      usage="shutdown", 
-      aliases=['logout', 'turnoff'])
-    @commands.is_owner()
-    @commands.guild_only()
-    async def shutdown(self, ctx):
-      
-        await ctx.message.delete()
-        
-        await ctx.send("I'm shutting down now 👋🏽", delete_after=5)
-        
-        await asyncio.sleep(1)
-        
-        await self.bot.logout()
+            if not answer:
+              #They failed to provide an answer
+              await member.send(f"You failed to answer: `{question}`\nBe quicker next time")
+              return
 
+            # We have a valid answer to a question, lets store it
+            applicationAnswers[applicationQuestions.index(question)] = answer
+            print(answer)
+
+        # We finished asking questions, lets check they want to submit this before sending it off
+        confirmation = await member.send("Are you sure you want to submit this application?")
+        await confirmation.add_reaction("✅")
+        await confirmation.add_reaction("❌")
+
+        reaction, user = await self.bot.wait_for(
+            "reaction_add", timeout=60.0, check=checkreact
+        )
+        if str(reaction.emoji) == "✅":
+            async with member.typing():
+                await member.send(
+                    "Thank you for applying! Your application will be sent to the Owner soon"
+                )
+
+                # We need to sus out whether or not to split the application into separate
+                # embeds or not due to embed ratelimits
+                descriptionChunks = {}
+
+                description = ""
+                for key, value in applicationAnswers.items():
+                    addition = f"{key+1}) {applicationQuestions[key]}\n{value}\n\n"
+
+                    if (len(description) + len(addition)) > 1850 and len(addition) <= 1850:
+                        # This is gonna be to big for 1 embed together, but its gucci on its own
+                        # so lets split it and make 2 embeds
+
+                        # Lets make the bigger of the two into a descriptionChunks and the other description
+                        if len(description) > len(addition):
+                            # the current description is bigger
+                            descriptionChunks[description] = False
+                            description = addition
+                        else:
+                            # the addition is bigger
+                            descriptionChunks[addition] = False
+                        continue
+
+                    elif len(addition) > 1850:
+                        # This line alone is to big for one embed description
+                        if len(addition) < 2850:
+                            # It can still technically fit into 1 embed
+                            partOne = addition[:1850]
+                            partTwo = addition[1850:]
+                            descriptionChunks[partOne] = {'multipleEmbeds': False, 'fieldValue': partTwo}
+                        else:
+                            # Technically, it has to be less then 4k words or
+                            # else we would not have been able to send ours
+                            # and vice versa so make 2 embeds
+                            partOne = addition[:1850]
+                            partTwo = addition[1850:]
+                            descriptionChunks[partOne] = False
+                            descriptionChunks[partTwo] = False
+                        continue
+
+                    else:
+                        # We should be safe to simply add this to our current
+                        # description and carry on as we are
+                        description += addition
+                        continue
+
+                else:
+                    if description != "":
+                        descriptionChunks[description] = False
+
+                for key, value in descriptionChunks.items():
+                    applicationEmbed = discord.Embed(
+                        title="Application Answers __**{MODERATOR}**__",
+                        description=key,
+                        color=discord.Color.dark_purple(),
+                        timestamp=datetime.utcnow()
+                    )
+                    applicationEmbed.set_author(
+                        name=f"Application taken by: {member.display_name}", icon_url=f"{member.avatar_url}"
+                    )
+                    applicationEmbed.set_footer(text=f"{member.display_name}")
+
+                    if value != False:
+                        # We need to add a field for the rest of the data
+                        applicationEmbed.add_field(name='\uFEFF', value=value['fieldValue'])
+                    
+                    channelnames = ['application']
+                    #Checks for a channel with an Application keyword to send this to
+                    channel = discord.utils.find(
+                      lambda channel:any(
+                        map(lambda c: c in channel.name, channelnames)), ctx.guild.text_channels)
+                    
+                    #Checks if a application channel doesn't exist    
+                    if not channel:
+                      otherchann = ['gener', 'chat', 'welc']
+                      newchann = discord.utils.find(
+                        lambda newchann:any(
+                          map(lambda n: n in newchann.name, otherchann)), ctx.guild.text_channels)
+                      await newchann.send("I can't seem to find the Application channel containing a keyword {`application`} to send everybody's applications")
+
+                    await channel.send(embed=applicationEmbed)
+
+        elif str(reaction.emoji) == "❌":
+            await member.send("Application won't be sent")
+
+
+async def GetMessage(
+    bot, member, contentOne="Default Message", contentTwo="\uFEFF", timeout=500
+):
+    """
+    This function sends an embed containing the params and then waits for a message to return
+
+    Params:
+     - bot (commands.Bot object) :
+     - member (member object) : Used for sending members the embed
+     
+     - Optional Params:
+        - contentOne (string) : Embed title
+        - contentTwo (string) : Embed description
+        - timeout (int) : Timeout for wait_for
+
+    Returns:
+     - msg.content (string) : If a message is detected, the content will be returned
+    or
+     - False (bool) : If a timeout occurs
+    """
+    embed = discord.Embed(title=f"{contentOne}", description=f"{contentTwo}", color=discord.Color.darker_grey())
+    async with member.typing():
+        await asyncio.sleep(2)
+        await member.send(embed=embed)
+    
+    try:
+        msg = await bot.wait_for(
+            "message",
+            timeout=timeout,
+            check=lambda message: message.author == member and message.guild == None)
+        if msg:
+            return msg.content
+    except asyncio.TimeoutError:
+        return False
+
+#•----------Setup/Add Cog----------•#
 def setup(bot):
     bot.add_cog(Misc(bot))
