30d210f94ca13ce5edcb8113eb32b9c1fd9b919f
Nunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-27 00:59:00 -0400

    Removed Commentary and Minor Changes

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -1,7 +1,14 @@
+
+#•----------Modules----------•#
 import discord
+
 from discord.ext import commands
+
 from discord import Embed
+
 import string
+
+#•----------Functions-----------•#
 
 async def on_bot_forbidden(self, ctx, bot, args2):
     """Handles Missing Bot Permissions Errors"""
@@ -105,64 +112,11 @@ async def on_not_owner(self, ctx, bot):
     else:
         print("Error: Error Handling Message Could Not Be Sent")
 
+#•-----------Class-----------•#
+
 class Errors(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    #All global errors
-    #@commands.Cog.listener()
-    #async def on_command_error(self, ctx, error):
-       # error = getattr(error, 'original', error)
-        #if isinstance(error, commands.CommandNotFound):
-            
-       #     await ctx.send('~~Command doesn\'t exist bro~~')
-       #     await ctx.message.add_reaction('⛔')
-
-       # elif isinstance(error, commands.BadArgument):
-         #   await ctx.send("Looks like you put in the wrong argument")
-        
-        #Checks for Bot Missing Perms
-       # elif isinstance(error, commands.BotMissingPermissions):
-          
-        #  embed = discord.Embed(
-         #   description="Seems like I'm missing some permissions", 
-         #   color=discord.Color.dark_red())
-            
-         # await ctx.send(embed=embed)
-        
-        #Checks if the Invoker is the Owner of the Bot
-      #  elif isinstance(error, commands.NotOwner):
-          
-       #   embed = discord.Embed(description="You're not the Owner of this bot dummy", color=discord.Color.dark_red)
-          
-        # await ctx.send(embed=embed)
-        
-        #elif isinstance(error, commands.NoPrivateMessage):
-            
-         #   await ctx.send('~~You can\'t use commands outside of the server~~')
-        
-      # elif isinstance(error, commands.CommandOnCooldown):
-            
-          #  guild = ctx.guild
-          #  if not guild:
-            #    await ctx.reinvoke()
-          #  else:
-               # var1 = error.retry_after
-               # var2 = int(var1)
-                
-              #  await ctx.send(f'You can\'t use this command for another {var2} seconds')
-        
-       # elif isinstance(error, commands.MissingPermissions):
-            
-           # poo = discord.Embed(description="You don't have the permissions to do that, idiot", color=discord.Color.dark_red())
-            
-           # await ctx.send(embed=poo, delete_after=5)
-        
-      #  elif isinstance(error, commands.MissingRequiredArgument):
-            
-         #   await ctx.send("Seems like you're forgetting to put something bruh")
-       # else:
-           # raise(error)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, args2):
