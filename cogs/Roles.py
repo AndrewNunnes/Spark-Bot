d78c1758a0f6b7d263b49a4f9d0c6940409b7820
Nunnes <68153058+AndrewNunnes@users.noreply.github.com>
2020-08-27 00:49:41 -0400

    Refactored Role Commands

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -1,7 +1,7 @@
-#•----------Modules----------•#
+----------Modules----------•#
 import discord
 
-from discord.ext.commands import command, Cog, guild_only, has_permissions, bot_has_permissions, BadArgument
+from discord.ext.commands import command, Cog, guild_only, has_permissions, bot_has_permissions, BadArgument, Greedy
 
 import asyncio
 
@@ -9,13 +9,15 @@ from datetime import datetime
 
 import typing
 
-from typing import Union
+from typing import Union, Optional
+
+#•----------Functions----------•#
 
 #•----------Commands----------•#
 
-class Role(Cog, name="Role Management"):
+class Role(Cog, name="Role Category"):
   
-    """😱 `{Commands for Managing Roles}`"""
+    """`{Commands for Managing Roles}`"""
   
     def __init__(self, bot):
         self.bot = bot
@@ -62,13 +64,15 @@ class Role(Cog, name="Role Management"):
     async def name_error(self, ctx, error):
       if isinstance(error, BadArgument):
       
-        e = discord.Embed(
-          color=discord.Color.dark_red(), 
-          description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role name"``\n• Valid Syntax: `{ctx.prefix}rolename <new_name> <role_name>`')
+          e = discord.Embed(
+              color=0x420000, 
+              description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role name"``\n• Valid Syntax: `{ctx.prefix}rolename <new_name> <role_name>`')
         
-        e.timestamp = datetime.utcnow()
+          e.timestamp = datetime.utcnow()
       
-        await ctx.send(embed=e)
+          await ctx.send(embed=e)
+      else:
+          raise(error)
       
     @command(
       name="rolecolor", 
@@ -78,7 +82,8 @@ class Role(Cog, name="Role Management"):
     @has_permissions(manage_roles=True)
     @bot_has_permissions(manage_roles=True)
     async def color(self, ctx, color: discord.Color, *, role: discord.Role):
-    
+        
+        #If they don't give a color
         if not color:
             await ctx.send("You gotta give a new color for the role")
             return
@@ -106,142 +111,93 @@ class Role(Cog, name="Role Management"):
     async def color_error(self, ctx, error):
       if isinstance(error, BadArgument):
       
-        e = discord.Embed(
-          color=discord.Color.dark_red(), 
-          description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role_name>"`\n• Valid Syntax: `{ctx.prefix}role color <color> <role_name>`')
+          e = discord.Embed(
+              color=0x420000, 
+              description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role_name>"`\n• Valid Syntax: `{ctx.prefix}role color <color> <role_name>`')
           
-        e.timestamp = datetime.utcnow()
+          e.timestamp = datetime.utcnow()
         
-        await ctx.send(embed=e)
+          await ctx.send(embed=e)
+      else:
+          raise(error)
     
     @command(
       name="addrole", 
       brief="{Add a Role to a Member}", 
-      usage="addrole <member> <role>")
+      usage="addrole <member> <role>", 
+      aliases=['addr', 'radd'])
     @guild_only()
     @has_permissions(manage_roles=True)
     @bot_has_permissions(manage_roles=True)
     async def add(self, ctx, member: discord.Member, *, role: discord.Role):
-    
+      
+        #If author doesn't give at least 1 member
+        if not member:
+            await ctx.send("You have to give a member to give a role to")
+            return
+
         await member.add_roles(role)
-    
-        await ctx.send(f"Successfully gave {role.mention} to: `{member}`")
-    
-    @add.error
-    async def add_error(self, ctx, error):
-      if isinstance(error, BadArgument):
+        
         e = discord.Embed(
-          color=discord.Color.dark_red(), 
-          description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"role_name"`\n• Valid Syntax: `{ctx.prefix}addrole <member> <role_name>`')
-          
+            color=0x420000, 
+            description=f"<:greenmark:738415677827973152> __*Successfully gave {role.mention} to {member.mention}*__")
+            
         e.timestamp = datetime.utcnow()
         
         await ctx.send(embed=e)
+
+    @add.error
+    async def add_error(self, ctx, error):
+      if isinstance(error, BadArgument):
+          e = discord.Embed(
+              color=0x420000, 
+              description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"role_name"`\n• Valid Syntax: `{ctx.prefix}addrole <member> <role_name>`')
+          
+          e.timestamp = datetime.utcnow()
+        
+          await ctx.send(embed=e)
+      else:
+          raise(error)
       
     @command(
       name="removerole", 
       brief="{Remove a Role from a Member}", 
-      usage="removerole <member> <role_name>", 
+      usage="removerole <member(s)> <role_name>", 
       aliases=['roleremove', 'rremove'])
     @guild_only()
     @has_permissions(manage_roles=True)
     @bot_has_permissions(manage_roles=True)
     async def remove(self, ctx, member: discord.Member, *, role: discord.Role):
-      
-        #Remove role from member
-        await member.remove_roles(role)
-      
-        await ctx.send(f"Successfully removed {role.mention} from: `{member}`")
-      
+        
+        #If a member isn't given
+        if not member:
+            await ctx.send("You have to give at least 1 member to remove this role from")
+            return
+        
+        else:
+            #Remove role from member
+            await member.remove_roles(role)
+        
+            e = discord.Embed(
+                color=0x420000, 
+                description=f"<:greenmark:738415677827973152> __*Successfully removed {role.mention} from {member.mention}*__")
+                
+            e.set_thumbnail(
+                url=member.avatar_url)
+
     @remove.error
     async def remove_error(self, ctx, error):
         if isinstance(error, BadArgument):
-        
-          e = discord.Embed(
-            color=discord.Color.dark_red(), 
-            description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role_name>"`\n• Valid Syntax: `{ctx.prefix}role remove <member> <role_name>`')
+
+            e = discord.Embed(
+                color=0x420000, 
+                description=f'Either:\n• Role wasn\'t found\n• Roles including spaces must be surrounded with `"<role_name>"`\n• Valid Syntax: `{ctx.prefix}role remove <member> <role_name>`')
             
-          e.timestamp = datetime.utcnow()
+            e.timestamp = datetime.utcnow()
         
-          await ctx.send(embed=e)
-          
-    @command(
-      name="roleinfo", 
-      brief="{Get Info on a Role}", 
-      usage="roleinfo <role>", 
-      aliases=['ri', 'rinfo'])
-    @guild_only()
-    async def info(self, ctx, *, role: discord.Role):
-          
-        #See when the role was created
-        role_created = f"{role.created_at.strftime('%a/%b %d/%Y • %I:%M %p')}"
-        
-        #List number of non-bots
-        humans = len(list(filter(lambda h: not h.bot, role.members)))
-        
-        #List number of bots
-        bots = len(list(filter(lambda b: b.bot, role.members)))
-        
-        #If there is more than 15 members
-        if len(role.members) > 25:
-            length = len(role.members) - 25
-        
-            human_list = f"{' , '.join(map(str, (member.mention for member in list(reversed(role.members))[:25])))} and **{length}** more"
-        #If there is less than 25 members
+            await ctx.send(embed=e)
         else:
-            human_list = f"{' , '.join(map(str, (member.mention for member in (list(reversed(role.members[1:]))))))}"
-        
-        #If there is no members 
-        human_lt = "No Members" if human_list == "" else human_list
-        
-        #Custom emojis
-        check = "<:greenmark:738415677827973152>"
-        x = "<:redmark:738415723172462723>"
-        
-        #Using emojis from above
-        #To show any bools
-        mentionable = check if role.mention else x
-        hoisted = check if role.hoist else x
-        
-        e = discord.Embed(
-            description=f"**General Info for {role.mention} {{Color In Hex > {role.color}}}**")
-      
-        #Make fields
-        fields = [("ID", role.id, True),
-        
-                  ("Misc", 
-                  f"\nMentionable? {mentionable}" +
-                  f"\nDisplays from Others? {hoisted}", True), 
-      
-                  ("Position", role.position, True), 
-                
-                  (f"Members w/{role.name} {{{len(role.members)}}}", 
-                  f"\nHumans: {humans}" +
-                  f"\nBots: {bots}", False), 
-                  
-                  (f"List of Members with this Role", human_lt, False)]
-                  
-        #Show when role was created
-        e.set_footer(
-            text=f"Role Created At | {role_created}")
-            
-        e.set_author(
-            name=f"Requested by {ctx.author}", 
-            icon_url=ctx.author.avatar_url)
-        
-        #Add fields     
-        for name, val, inl in fields:
-            e.add_field(
-                name=name, 
-                value=val, 
-                inline=inl)
-      
-        await ctx.send(embed=e)
-      
-    @info.error
-    async def info_error(self, ctx, error):
-        if isinstance(error, BadArgument):
-            await ctx.send("That isn't a valid role")
+            raise(error)
           
     @command(
       name="rolelist", 
@@ -289,16 +245,36 @@ class Role(Cog, name="Role Management"):
     @bot_has_permissions(use_external_emojis=True)
     async def perms(self, ctx, *, role: discord.Role):
     
-        #Iterating through list of perms
-        perms = [f'{perm.title().replace("_", " ")} {("= <:greenmark:738415677827973152>" if value else "= <:redmark:738415723172462723>")}' for perm, value in role.permissions]
-    
-        #Make and send embed
+        #perms = [f'{perm.title().replace("_", " ")} {("= <:greenmark:738415677827973152>" if value else "= <:redmark:738415723172462723>")}' for perm, value in role.permissions]
+        
+        #Make embed
         e = discord.Embed(
-            color=discord.Color.darker_grey(), 
-            title=f"Permissions for: `{role.name}`", 
-            description="\n\n".join(perms))
-    
-        e.timestamp = datetime.datetime.utcnow()
+            description=f"__*Permissions for {role.mention} {{*Color in Hex: {role.color}*}}*__")
+        
+        #Iterate through list of perms
+        for perm, value in role.permissions:
+            
+            #Use our custom emojis
+            #To show if perms are true or not
+            green_red = f'{("True <:greenmark:738415677827973152>" if value else "False <:redmark:738415723172462723>")}'
+            
+            #Make fields
+            fields = [(f'{perm.title().replace("_", " ")}', green_red, True)]
+            
+            #Add fields
+            for n, v, i in fields:
+                e.add_field(
+                    name=n, 
+                    value=v, 
+                    inline=i)
+            
+        e.set_thumbnail(
+            url=ctx.author.avatar_url)
+            
+        e.set_footer(
+            text=f"Requested by {ctx.author}")
+  
+        e.timestamp = datetime.utcnow()
       
         await ctx.send(embed=e)
       
@@ -306,6 +282,8 @@ class Role(Cog, name="Role Management"):
     async def perms_error(self, ctx, error):
         if isinstance(error, BadArgument):
             await ctx.send("That isn't a valid role")
+        else:
+            raise(error)
           
     @command(
         name="createrole", 
@@ -314,14 +292,10 @@ class Role(Cog, name="Role Management"):
     @guild_only()
     @has_permissions(manage_roles=True)
     @bot_has_permissions(manage_roles=True)
-    async def create(self, ctx, name=None, color: discord.Color=None, *, reason):
+    async def create(self, ctx, name=None, color: discord.Color=None, *, reason: Optional[str]="No Reason Provided"):
     
         guild = ctx.guild
-        
-        if reason is None:
-            await ctx.send("You need to give a reason for creating this role")
-            return
-    
+
         if name is None:
             await ctx.send("You have to give a name for the role")
             return
